cffd8806f5c0e349adfa27447980a54fac3219d9
Robby Bush <crobertbush@gmail.com>
2022-05-10 15:57:54 -0400

    js puzzle complete gg ez

--- app.js
+++ app.js
@@ -2,61 +2,61 @@ var shop = [
     {
         title: 'Tigers',
         image: './img/gerbile.jpg',
-        price: '$7,500-15,000'
-        description
+        price: '$7,500-15,000',
+        description: "a tiger"
     },
     {
         title: 'Tigers',
         image: './img/gerbile.jpg',
-        price: '$7,500-15,000'
-        description
+        price: '$7,500-15,000',
+        description: "a tiger"
     },
     {
         title: 'Tigers',
         image: './img/gerbile.jpg',
-        price: '$7,500-15,000'
-        description
+        price: '$7,500-15,000',
+        description: "a tiger"
     },
     {
         title: 'Tigers',
         image: './img/gerbile.jpg',
-        price: '$7,500-15,000'
-        description
+        price: '$7,500-15,000',
+        description: "a tiger"
     },
     {
         title: 'Tigers',
         image: './img/gerbile.jpg',
-        price: '$7,500-15,000'
-        description
+        price: '$7,500-15,000',
+        description: "a tiger"
     },
     {
         title: 'Tigers',
         image: './img/gerbile.jpg',
-        price: '$7,500-15,000'
-        description
+        price: '$7,500-15,000',
+        description: "a tiger"
     },
     {
         title: 'Tigers',
         image: './img/gerbile.jpg',
-        price: '$7,500-15,000'
-        description
+        price: '$7,500-15,000',
+        description: "a tiger"
     },
     {
         title: 'Tigers',
         image: './img/gerbile.jpg',
-        price: '$7,500-15,000'
-        description
+        price: '$7,500-15,000',
+        description: "a tiger"
     }
     
     ]
     
     var postHTML = " "
   
-    for (var i=0; i < ; i++){
-        var heading = '<div class="product ' + '"><span><h5>' + shop[i].title + '</h5>'
-        var image = '<img src="' + shop[i] + '"/'
-        var price = '<p> $' + .price + '</p></span>'
-        var description = '<div class=""><p>'+ .description + '</p><button type="button" class="btn btn-warning"> add to cart</button></div></div>'
+    for (var i=0; i < shop.length; i++){
+        var heading = '<div class="card product col-sm-6 col-md-3 col-lg-3 pb-5 ' + '"><span><h5>' + shop[i].title + '</h5>'
+        var image = '<img src="' + shop[i].image + '"/'
+        var price = '<p> $' + shop[i].price + '</p></span>'
+        var description = '<div class=""><p>'+ shop[i].description + '</p><button type="button" class="btn btn-warning"> add to cart</button></div></div>'
         var concatThis = heading + image + price + description;
         postHTML = postHTML + concatThis
     }
