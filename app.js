f9901abd9168a45a64ca895346ccf125c7af5edc
Robby Bush <crobertbush@gmail.com>
2022-05-10 16:28:09 -0400

    update

--- app.js
+++ app.js
@@ -1,51 +1,75 @@
 var shop = [
     {
-        title: 'Tigers',
-        image: './img/gerbile.jpg',
-        price: '$7,500-15,000',
-        description: "a tiger"
+        title: 'Food',
+        image: './img/banner.avif',
+        price: '3.50',
+        description: "Organic Food"
     },
     {
-        title: 'Tigers',
-        image: './img/gerbile.jpg',
-        price: '$7,500-15,000',
-        description: "a tiger"
+        title: 'Food',
+        image: './img/banner.avif',
+        price: '3.50',
+        description: "Organic Food"
     },
     {
-        title: 'Tigers',
-        image: './img/gerbile.jpg',
-        price: '$7,500-15,000',
-        description: "a tiger"
+        title: 'Food',
+        image: './img/banner.avif',
+        price: '3.50',
+        description: "Organic Food"
     },
     {
-        title: 'Tigers',
-        image: './img/gerbile.jpg',
-        price: '$7,500-15,000',
-        description: "a tiger"
+        title: 'Food',
+        image: './img/banner.avif',
+        price: '3.50',
+        description: "Organic Food"
     },
     {
-        title: 'Tigers',
-        image: './img/gerbile.jpg',
-        price: '$7,500-15,000',
-        description: "a tiger"
+        title: 'Food',
+        image: './img/banner.avif',
+        price: '3.50',
+        description: "Organic Food"
     },
     {
-        title: 'Tigers',
-        image: './img/gerbile.jpg',
-        price: '$7,500-15,000',
-        description: "a tiger"
+        title: 'Food',
+        image: './img/banner.avif',
+        price: '3.50',
+        description: "Organic Food"
     },
     {
-        title: 'Tigers',
-        image: './img/gerbile.jpg',
-        price: '$7,500-15,000',
-        description: "a tiger"
+        title: 'Food',
+        image: './img/banner.avif',
+        price: '3.50',
+        description: "Organic Food"
     },
     {
-        title: 'Tigers',
-        image: './img/gerbile.jpg',
-        price: '$7,500-15,000',
-        description: "a tiger"
+        title: 'Food',
+        image: './img/banner.avif',
+        price: '3.50',
+        description: "Organic Food"
+    },
+    {
+        title: 'Food',
+        image: './img/banner.avif',
+        price: '3.50',
+        description: "Organic Food"
+    },
+    {
+        title: 'Food',
+        image: './img/banner.avif',
+        price: '3.50',
+        description: "Organic Food"
+    },
+    {
+        title: 'Food',
+        image: './img/banner.avif',
+        price: '3.50',
+        description: "Organic Food"
+    },
+    {
+        title: 'Food',
+        image: './img/banner.avif',
+        price: '3.50',
+        description: "Organic Food"
     }
     
     ]
@@ -53,8 +77,8 @@ var shop = [
     var postHTML = " "
   
     for (var i=0; i < shop.length; i++){
-        var heading = '<div class="card product col-sm-6 col-md-3 col-lg-3 pb-5 ' + '"><span><h5>' + shop[i].title + '</h5>'
-        var image = '<img src="' + shop[i].image + '"/'
+        var heading = '<div class="card product col-sm-6 col-md-4 col-lg-3 pb-5 ' + '"><span><h5>' + shop[i].title + '</h5>'
+        var image = '<img class="img-fluid" src="' + shop[i].image + '"/'
         var price = '<p> $' + shop[i].price + '</p></span>'
         var description = '<div class=""><p>'+ shop[i].description + '</p><button type="button" class="btn btn-warning"> add to cart</button></div></div>'
         var concatThis = heading + image + price + description;
